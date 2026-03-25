3923d07c37446c1f98f4c7098110987a1ac1354e
Davemafy <111160000+Davemafy@users.noreply.github.com>
2026-03-25 22:09:02 +0100

    Rename .jsx to .tsx

--- src/routes/app/_auth/_signup/route.jsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute, Outlet } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/app/_auth/_signup')({
-  component: Layout,
-})
-
-function Layout() {
-  return <Outlet />
-}
