384e4af4e3e6486bc2a4b63b8c2e2b40e0248be4
Davemafy <111160000+Davemafy@users.noreply.github.com>
2026-03-26 01:09:02 +0100

    Fix mobile login button bug

---
+++ src/index.d.ts
@@ -0,0 +1,12 @@
+interface CourseProps {
+  id: string;
+  img: string;
+  title: string;
+  rating: number;
+  user_id: string;
+  duration: sting;
+  subtitle: string;
+  progress: number;
+  created_at: string;
+  instructor: string;
+}
