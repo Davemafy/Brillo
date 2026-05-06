bab122b7ac30109d6b4af021f1059afbf7e42c46
Davemafy <111160000+Davemafy@users.noreply.github.com>
2026-05-07 00:49:35 +0100

    Add edit feature

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -1,6 +1,6 @@
-import { Pencil, Trash } from "lucide-react";
+import { EarIcon, Pencil, Save, Trash, X, XCircle } from "lucide-react";
 import { supabase } from "../superbaseClient";
-import type { Dispatch, SetStateAction } from "react";
+import { useState, type Dispatch, type SetStateAction } from "react";
 import type React from "react";
 
 interface NoteCardProps {
@@ -11,6 +11,10 @@ interface NoteCardProps {
 }
 
 const NoteCard = ({ note, className, notes, setNotes }: NoteCardProps) => {
+  const [editing, setEditing] = useState(false);
+  const [titleValue, setTitleValue] = useState(note.title);
+  const [descValue, setDescValue] = useState(note.description);
+
   const deleteNote = async (noteId: string) => {
     const { error } = await supabase.from("notes").delete().eq("id", noteId);
 
@@ -23,23 +27,67 @@ const NoteCard = ({ note, className, notes, setNotes }: NoteCardProps) => {
     }
   };
 
+  function saveEdit() {
+    setEditing(false);
+    
+  }
+
+  function cancelEdit() {
+    setEditing(false);
+    setTitleValue(note.title)
+    setDescValue(note.description)
+  }
+
   return (
     <li
-      className={`flex relative cursor-pointer outline w-full bg-[#f9f9f9] flex-col p-6 px-6 gap-2 border outline-accent border-grey rounded-2xl ${className || ""}`}
+      className={`${editing ? "border-black bg-linear-120 from-[#f9f9f9] to-[#FFF9E6] " : "border-grey bg-[#f9f9f9] "} flex relative cursor-pointer outline w-full flex-col p-6 px-6 gap-2 border outline-accent rounded-2xl ${className || ""}`}
     >
       <div className="flex mb-2 justify-between items-center ">
         <p className="text-[0.5rem] p-2 px-3 font-semibold bg-neutral-200 rounded-2xl">
           {note.course_title.toUpperCase()}
         </p>
         <div className="flex gap-2">
+          {!editing ? (
+            <button
+              style={
+                {
+                  "--tooltip-text": '"Edit"',
+                } as React.CSSProperties
+              }
+              onClick={() => setEditing(true)}
+              className="tooltip border-gray-200 ml-auto"
+            >
+              <Pencil size={15} className="hover:text-orange-500" />
+            </button>
+          ) : (
+            <>
+              <button
+                style={
+                  {
+                    "--tooltip-text": '"Cancel"',
+                  } as React.CSSProperties
+                }
+                onClick={() => cancelEdit()}
+                className="tooltip border-gray-200 ml-auto"
+              >
+                <X size={15} className="hover:text-red-500" />
+              </button>
+              <button
+                style={
+                  {
+                    "--tooltip-text": '"Save"',
+                  } as React.CSSProperties
+                }
+                onClick={() => saveEdit()}
+                className="tooltip border-gray-200 ml-auto"
+              >
+                <Save size={15} className="hover:text-green-600" />
+              </button>
+            </>
+          )}
           <button
-            style={{ "--tooltip-text": '"Edit"' } as React.CSSProperties}
-            className="tooltip border-gray-200 ml-auto"
-          >
-            <Pencil size={15} />
-          </button>
-          <button
-            className="hover:bg-red-500"
+            style={{ "--tooltip-text": '"Delete"' } as React.CSSProperties}
+            className=" tooltip hover:text-red-500 "
             onClick={(e) => {
               deleteNote(note.id);
             }}
@@ -50,9 +98,28 @@ const NoteCard = ({ note, className, notes, setNotes }: NoteCardProps) => {
       </div>
       <div className="flex h-full mb-2 justify-between">
         <div className="h-full">
-          <h4 className="font-bold text-base">{note.title}</h4>
+          <h4 className="font-bold text-base">
+            {editing ? (
+              <input
+                value={titleValue}
+                autoFocus
+                className="border-0 p-0 bg-transparent ring-0"
+                onChange={(e) => setTitleValue(e.currentTarget.value)}
+              />
+            ) : (
+              note.title
+            )}
+          </h4>
           <p className="flex-1 text-sm mt-2 max-w-[60ch] text-neutral-800 truncate-multiline leading-6 ">
-            {note.description}
+            {editing ? (
+              <input
+                value={descValue}
+                className="border-0 p-0 text-sm bg-transparent ring-0"
+                onChange={(e) => setDescValue(e.currentTarget.value)}
+              />
+            ) : (
+              note.description
+            )}
           </p>
         </div>
       </div>
